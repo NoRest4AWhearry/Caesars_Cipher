653e61ce790656c421fe453f0936cb1c5c361403
NoRest4AWhearry <awhearry@gmail.com>
2014-04-06 04:27:44 -0400

    Change first if conditional to case/when

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,15 @@
-def caesar_cipher(string, num)
-	string.each_char do |letter|
-		ord = letter.ord
-		if ord >= 65 && ord <= 90 || ord >= 97 && ord <= 122
-			offset = ord + num
-			offset < 65 || offset > 90 && offset < 97 || offset > 122 ? offset = offset - 26 : nil
-		else
-			offset = ord
+def caesar_cipher(string,num)
+	string.each_char do |i|
+		ord = i.ord
+		case ord
+			when (65..90),(97..122)
+				offset = ord + num
+				offset < 65 || offset > 90 && offset < 97 || offset > 122 ? offset = offset - 26 : nil
+			else
+				offset = ord
 		end
 		print offset.chr
 	end
 end
-string = 'What a string!'
-caesar_cipher(string, 5)
+string='What a string!'
+caesar_cipher(string,5)
