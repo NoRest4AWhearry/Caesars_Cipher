138ddbd2a2e32862453484094a994a9df1a1c850
NoRest4AWhearry <awhearry@gmail.com>
2014-04-06 04:57:42 -0400

    Minimized code lines used

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,8 @@
 def caesar_cipher(string,num)
-	string.each_char do |i|
-		ord = i.ord
-		case ord
-			when (65..90),(97..122)
-				offset = ord + num
-				offset < 65 || offset > 90 && offset < 97 || offset > 122 ? offset = offset - 26 : nil
-			else
-				offset = ord
-		end
-		print offset.chr
-	end
+	string.each_char { |i| ord = i.ord
+	case ord when (65..90),(97..122) then offset = ord + num
+	(offset > 90 && offset < 97 ? offset = offset - 26 : nil) else offset = ord end
+	print offset.chr }
 end
 string='What a string!'
 caesar_cipher(string,5)
