3c7aa0bcd2edb60afb57392c5d84312f7791d96d
NoRest4AWhearry <awhearry@gmail.com>
2014-04-06 01:24:44 -0400

    add ceasar cipher code

---
+++ caesar_cipher.rb
@@ -0,0 +1,14 @@
+def ceasar_cipher(string, num)
+	string.each_char do |letter|
+		ord = letter.ord
+		if ord >= 65 && ord <= 90 || ord >= 97 && ord <= 122
+			offset = ord + num
+			offset < 65 || offset > 90 && offset < 97 || offset > 122 ? offset = offset - 26 : nil
+		else
+			offset = ord
+		end
+		print offset.chr
+	end
+end
+string = 'What a string!'
+ceasar_cipher(string, 5)
