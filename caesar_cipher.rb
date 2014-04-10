f82a2dcae45ca43b50476d7bb77a6b6c2d53ea66
NoRest4AWhearry <awhearry@gmail.com>
2014-04-09 23:21:36 -0400

    Change each_char to each_byte

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,9 @@
 def caesar_cipher(string,num)
-	string.each_char { |i| ord = i.ord
-	case ord when (65..90),(97..122) then offset = ord + num
-	(offset > 90 && offset < 97 ? offset = offset - 26 : nil) else offset = ord end
+	string.each_byte { |i|
+	case i when (65..90),(97..122) then offset = i + num
+	(offset > 90 && offset < 97 ? offset = offset - 26 : nil) else offset = i end
 	print offset.chr }
 end
 string='What a string!'
 caesar_cipher(string,5)
+
